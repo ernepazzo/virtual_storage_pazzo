fa7dcd835fa114a8f2af7302b56385d684ee3dda
Ernesto Mejias <99213980+ernepazzo@users.noreply.github.com>
2023-07-09 14:02:26 -0400

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(email: 'ernepazzo@virtualstoragepazzo.com', username: 'ernepazzo', whatsapp: 53867624, admin: true)
