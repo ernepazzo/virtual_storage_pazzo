08498339c749888e95db9b96bc57e25abfbaf699
Ernesto Mejias <ernepazzo1212@gmail.com>
2024-04-03 23:46:59 -0400

    cambiando ruta de inicio

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'main#index'
+  root 'products#index'
   namespace :authentication, path: '', as: '' do
     resources :users, only: [:new, :create], path: '/register', path_names: { new: '/' }
     resources :sessions, only: [:new, :create, :destroy], path: '/login', path_names: { new: '/' }
