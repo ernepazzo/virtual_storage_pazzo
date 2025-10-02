14639b32e57e15c1c05b258404c5ccd28b313347
Ernesto <ernepazzo1212@gmail.com>
2025-10-02 11:44:12 -0400

    a ver

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,10 +1,22 @@
 const path = require('path')
 const rails = require('esbuild-rails')
+const esbuild = require('esbuild')
 
-require("esbuild").build({
+const watch = process.argv.includes('--watch')
+const config = {
   entryPoints: ["application.js"],
   bundle: true,
   outdir: path.join(process.cwd(), "app/assets/builds"),
   absWorkingDir: path.join(process.cwd(), "app/javascript"),
   plugins: [rails()],
-}).catch(() => process.exit(1))
+}
+
+if (watch) {
+  esbuild.context(config).then(context => {
+    context.watch().then(() => {
+      console.log('watching for changes...')
+    })
+  }).catch(() => process.exit(1))
+} else {
+  esbuild.build(config).catch(() => process.exit(1))
+}
