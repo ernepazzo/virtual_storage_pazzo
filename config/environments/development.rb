46060322b6c4b84d4782c260540265e6ac5ea1f1
Livan <livanmay27@gmail.com>
2025-10-11 00:12:43 -0400

    Agreganndo smtp de envio de correos

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,7 +70,26 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   # Letter Opener
-  config.action_mailer.delivery_method = :letter_opener
+  # config.action_mailer.delivery_method = :letter_opener
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # Send with Gmail
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.default_url_options = {
+    host: 'localhost:3000'
+  }
+
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'google.com',
+    user_name: 'livanmay27@gmail.com',
+    password: 'kcjo zxdb kvin trco',
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 end
