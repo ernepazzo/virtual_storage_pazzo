0335ffe87e1e86d660b9e7a1e279984f5cdf0b56
Ernesto <ernepazzo1212@gmail.com>
2025-10-16 11:07:39 -0400

    De cuando uni con livan roles, accesos y permisos

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -9,7 +9,7 @@
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
 #  category_id :bigint           not null
-#  user_id     :bigint
+#  user_id     :bigint           not null
 #
 # Indexes
 #
@@ -19,7 +19,7 @@
 # Foreign Keys
 #
 #  fk_rails_...  (category_id => categories.id)
-#  fk_rails_...  (user_id => users.id)
+#  fk_rails_...  (user_id => users_r.id)
 #
 require "test_helper"
 
